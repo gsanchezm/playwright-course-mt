0e15cc1f61a67830ccc5165d45c4b25d7bd3c995
jimena-henao <jimena.henao@gmail.com>
2026-06-29 18:57:13 -0500

    Update CatalogPage with new method SearchPizza

--- pages/CatalogPage.ts
+++ pages/CatalogPage.ts
@@ -1,5 +1,7 @@
 import { BasePage } from "./BasePage";
 
-export class CatalogPage extends BasePage{
-    
+export class CatalogPage extends BasePage {
+  searchPizza(name: string): this {
+    return this.step(() => this.tid("search-pizza").fill(name));
+  }
 }
